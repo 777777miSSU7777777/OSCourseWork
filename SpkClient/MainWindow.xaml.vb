03c876110afd861b9788987dbda2eaff323a0ca6
777777miSSU7777777 <6x7missu7x7@gmail.com>
2018-12-09 21:18:24 +0300

    Added handlers for encrypt & decrypt buttons

--- SpkClient/MainWindow.xaml.vb
+++ SpkClient/MainWindow.xaml.vb
@@ -1,6 +1,21 @@
 ﻿Imports MahApps.Metro.Controls
+Imports System.IO
 
 Class MainWindow
     Inherits MetroWindow
+
+    Private Sub EncryptButton_Click(sender As Object, e As RoutedEventArgs) Handles EncryptButton.Click
+        Dim assembly = Reflection.Assembly.LoadFile(Path.GetFullPath("SpkCOM.dll"))
+        Dim comType As Type = Type.GetTypeFromProgID("SpkCOM.SpkCryptoService")
+        Dim cryptoService = Activator.CreateInstance(comType)
+        EncryptedInput.Text = cryptoService.Encrypt(SourceInput.Text, EncryptKey.Text)
+    End Sub
+
+    Private Sub DecryptButton_Click(sender As Object, e As RoutedEventArgs) Handles DecryptButton.Click
+        Dim assembly = Reflection.Assembly.LoadFile(Path.GetFullPath("SpkCOM.dll"))
+        Dim comType As Type = Type.GetTypeFromProgID("SpkCOM.SpkCryptoService")
+        Dim cryptoService = Activator.CreateInstance(comType)
+        DecryptedOutput.Text = cryptoService.Decrypt(EncryptedInput.Text, DecryptKey.Text)
+    End Sub
 
 End Class
